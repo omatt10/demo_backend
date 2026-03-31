465c1fdb54df0b52f2c85b26fe3575b52ba8746f
omatt10 <oliviapinkie@gmail.com>
2026-03-31 12:42:03 -0400

    back end

--- app.js
+++
@@ -1,9 +0,0 @@
-var http = require("http");
-const port = process.env.PORT || 3001; 
-
-http
-  .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Hello World!");
-  })
-  .listen(port);
